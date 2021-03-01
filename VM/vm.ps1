78ee14349a8dea954ce2f1bfc96084169b8cfa96
chandde <dengydongn@gmail.com>
2021-02-28 23:53:23 -0800

    fix dvd attach issue:

--- VM/vm.ps1
+++ VM/vm.ps1
@@ -10,16 +10,38 @@ function CreateVM
         if ($i.SwitchType -ceq "External")
         {
             $ExternalSwitchName = $i.Name
+            break
         }
     }
 
-    Write-Host($ExternalSwitchName)
+    if ($ExternalSwitchName -eq $null)
+    {
+        Write-Error "Cannot find any external virtual switch" -ErrorAction Stop
+    }
 
-    New-VM -Name $VMName -MemoryStartupBytes 4GB -BootDevice VHD -NewVHDPath $VMRootFolder'\'$VMName'\'$VMName'.vhdx' -Path $VMRootFolder'\'$VMName -NewVHDSizeBytes $VHDSize -Generation $VMGen -Switch $ExternalSwitchName
-    Add-VMDvdDrive -VMName $VMName -Path $ISOPath
-    $dvd = Get-VMDvdDrive -VMName $VMName
-    Set-VMFirmware $VMName -FirstBootDevice $dvd
+    $vm = New-VM -Name $VMName -MemoryStartupBytes 4GB -BootDevice VHD -NewVHDPath $VMRootFolder'\'$VMName'\'$VMName'.vhdx' -Path $VMRootFolder'\'$VMName -NewVHDSizeBytes $VHDSize -Generation $VMGen -Switch $ExternalSwitchName
+    
+    if ($vm -eq $null)
+    {
+        Write-Error "Fail to create VM" -ErrorAction Stop
+    }
+
+    Set-VMDvdDrive -VMName $VMName -Path $ISOPath
+
+    if ($VMGen -eq 2)
+    {
+        $dvd = Get-VMDvdDrive -VMName $VMName
+    
+        Set-VMFirmware $VMName -FirstBootDevice $dvd
+    }
+
+    if ($VMGen -eq 1)
+    {
+        Set-VMBios $VMName -StartupOrder @("CD", "IDE", "LegacyNetworkAdapter", "Floppy")
+    }
+    
     Set-VMProcessor $VMName -Count $CpuCount
+    
     Start-VM $VMName
 }
 
@@ -33,16 +55,26 @@ function ImportVM
         if ($i.SwitchType -ceq "External")
         {
             $ExternalSwitchName = $i.Name
+            break
         }
+    }
+
+    if ($ExternalSwitchName -eq $null)
+    {
+        Write-Error "Cannot find any external virtual switch" -ErrorAction Stop
     }
 
     $vmcx = Get-ChildItem $OldVMPath"\*.vmcx" -Recurse
 
-    Write-Host($vmcx)
+    if ($vmcx -eq $null)
+    {
+        Write-Error "Cannot find .vmcx file in "$OldVMPath -ErrorAction Stop
+    }
 
     $newVM = Import-VM -Path $vmcx.FullName -VhdDestinationPath $NewVMPath -Copy -GenerateNewId -VirtualMachinePath $NewVMPath
     
     Add-VMNetworkAdapter -VMName $newVM.Name -SwitchName $ExternalSwitchName
+
 
     $importedName = $newVM.Name
     $changedName = $importedName + $(Get-Date)
